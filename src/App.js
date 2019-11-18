d4e16a7b1f8aa3697f5517ecb8256a478d9aa5f1
Aden Collinge <aden.collinge@gmail.com>
2019-11-17 19:22:53 -0500

    asdfasdf

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { render } from 'react-dom'
+import CustomTimeline from './CustomTimeLine'
+import 'react-calendar-timeline/lib/Timeline.css'
+import Rdv from './Rdv';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Trendy colours 
+//#fc8780
+//#f8dcca
+//#7a9ba6
+//#3a4a62
+
+const GRID_INTERVAL_X = 25;
+const ROW_HEIGHT = 200;
+const viewWidth = 2000;
+
+
+const viewStartDate = new Date('1995-12-10T03:24:00');
+const viewEndDate = new Date('1995-12-17T03:24:00');
+const rdv2StartDate = new Date('1995-12-11T03:24:00');
+
+const rdvStartDate = new Date('1995-12-11T22:24:00');
+
+const getLeftOffset = (
+  viewWidth,
+  rdvStartDate,
+  viewStartDate,
+  viewEndDate) => {
+  const diff = viewEndDate - viewStartDate;
+  const rdvDiff = viewEndDate - rdvStartDate;
+  const offsetRatio = rdvDiff / diff
+
+  return viewWidth - (offsetRatio * viewWidth);
 }
 
+const Column = (index, gridSpacingY)=>(
+  <div className='dummyColumns'
+    style={{
+      height: ROW_HEIGHT + 'px',
+      width: viewWidth + 'px',
+      //borderStyle: 'solid',
+      // borderWidth: '3px 0px 0px 3px',
+      // borderColor: 'white',
+      position: 'aboslute',
+      margin:'6px',
+      padding:0,
+      backgroundColor: (index % 2 )? 'rgb(226, 226, 226)' : 'white',
+
+    }}></div>)
+
+
+const App = () => (
+  <div className='App'>
+  <div style={{ height: '300px'}} className='header' />
+    <div style = {{margin:'100px'}}>
+    {/* <CustomTimeline /> */}
+    <Rdv offset={getLeftOffset(viewWidth, rdvStartDate, viewStartDate, viewEndDate)} gridSpacingX={GRID_INTERVAL_X} gridSpacingY={ROW_HEIGHT} />
+    <Rdv offset={getLeftOffset(viewWidth, rdv2StartDate, viewStartDate, viewEndDate)} gridSpacingX={GRID_INTERVAL_X} gridSpacingY={ROW_HEIGHT} />
+    {Array(10).fill('').map((nothing,i)=>Column(i, ROW_HEIGHT))}
+    </div>
+  </div>
+)
 export default App;
---
+++ src/Rdv.js
@@ -0,0 +1,137 @@
+
+
+import React from 'react';
+import { Resizable, ResizableBox } from 'react-resizable';
+// import 'style-loader!css-loader!../css/styles.css';
+// import 'style-loader!css-loader!./test.css';
+import './rdv.css'
+
+import Draggable from 'react-draggable'; // The default
+// import {DraggableCore} from 'react-draggable'; // <DraggableCore>
+// import Draggable, {DraggableCore} from 'react-draggable';
+
+
+
+export default class Rdv extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            width: 200,
+            height: this.props.gridSpacingY,
+            offset: 10,
+            selected: false,
+            moveDisabled: false,
+            activeDrags: 0,
+            deltaPosition: {
+              x: 0, y: 0
+            },
+        };
+    }
+
+    handleDrag = (e, ui) => {
+        const {x, y} = this.state.deltaPosition;
+        this.setState({
+          deltaPosition: {
+            x: x + ui.deltaX,
+            y: y + ui.deltaY,
+          }
+        });
+      };
+    
+      onDragStart = () => {
+        this.setState({activeDrags: ++this.state.activeDrags});
+      };
+    
+      onDragStop = () => {
+        this.setState({activeDrags: --this.state.activeDrags});
+      };
+
+
+    onClick = () => {
+        this.setState({ selected: !this.state.selected });
+    };
+
+    onResize = (event, { element, size, handle }) => {
+        this.setState({ width: size.width, height: size.height });
+    };
+
+    onResizeStart = () => {
+        this.setState({ moveDisabled: true })
+    }
+
+
+    //this will be where we could change calculate the new end date of the RDV; 
+    onResizeStop = (event, data ) => {
+        // this data.size.width does not represent the actually width rendered to the screen, 
+        // we'll have to caluclate the rounding in the same way the do for the grid in 'ResizableBox', 
+        const newWidth = data.size.width;
+        this.setState({width:newWidth})
+        this.setState({ selected: !this.state.selected });
+    }
+
+    render() {
+        const { offset, gridSpacingX, gridSpacingY} = this.props;
+        const { moveDisabled } = this.state.selected
+
+
+        const rdvStyle = {
+            position: 'absolute',
+            left: offset,
+            background: '#00A8A8',
+            boxShadow: this.state.selected ? '15px 15px rgba(0, 0, 0, 0.5)' : '10px 10px rgba(0, 0, 0, 0.5)',
+            margin: '-15px'
+        }
+
+        const lockIndicatorStyle = {
+            position: 'absolute',
+            right: '0px',
+            bottom: '10px',
+        }
+
+        const getHandle = () => {
+            //using false here to default to the built in handle. Eventually this could be a custom handle of somekind
+            const handle = false;
+            return this.state.selected ? handle : (<div />)
+        }
+
+
+    const dragHandlers = {onStart: this.onDragStart, onStop: this.onDragStop};
+    const {deltaPosition,} = this.state;
+
+
+        return (
+            <div >
+                <div className="layoutRoot">
+                    <Draggable
+                        grid={[gridSpacingX, gridSpacingY]}
+                        disabled={this.state.selected}
+                        onDrag={this.handleDrag}
+                        {...dragHandlers}
+                    >
+                        <div style={rdvStyle} >
+                            <ResizableBox className="box"
+                                handle={getHandle()}
+                                onResizeStop={this.onResizeStop}
+                                handleSize={[8, 8]} 
+                                axis={this.state.selected ? "x" : 'none'} 
+                                width={200}
+                                height={200}
+                                draggableOpts={{ grid: [gridSpacingX, gridSpacingX] }}
+                            >
+                                <div style= {{margin:'20px'}}>TERRY'S BANK</div>
+                                <div style= {{margin:'10px'}}> width: {this.state.width}</div>
+                                <div style= {{margin:'10px'}}>x: {deltaPosition.x.toFixed(0)}</div>
+                                <div style= {{margin:'10px'}}>y: {deltaPosition.y.toFixed(0)}</div>
+                                <div style= {{margin:'10px'}}>y-index: {deltaPosition.y.toFixed(0) / gridSpacingY}</div>
+                                <span className="lock-indicator" style={lockIndicatorStyle} onClick={this.onClick}> {this.state.selected ? '🔓' : '🔒'}</span>
+                            </ResizableBox>
+
+                        </div>
+                    </Draggable>
+                </div>
+            </div>
+        );
+    }
+}
